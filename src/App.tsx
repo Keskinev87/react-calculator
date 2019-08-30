c3f87add50604e910d0e701dc7110827d5a48cbb
Yavor Keskinev <yavor_keskinev@abv.bg>
2019-08-30 18:39:02 +0300

    The css styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Calculator from './components/Calculator';
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      Welcome
+      <header><span>ReactJS-based Calculator App</span></header>
+      <Calculator />
+      <footer></footer>
     </div>
   );
 }
